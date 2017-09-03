36ae7fbb1ceaab707357f3b966067785c73972d8
Sam Leatherdale <SamLeatherdale@users.noreply.github.com>
2017-09-03 19:30:49 +1000

    Initial commit

---
+++ js/gridgame.js
@@ -0,0 +1,195 @@
+var Board = (function () {
+    function Board(rows, columns, cell_size, canvas) {
+        this.duration = 0;
+        this.moves = 0;
+        this.state = GameState.Initial;
+        Cell.initialize();
+        this.rows = rows;
+        this.columns = columns;
+        this.cell_size = cell_size;
+        this.bounds = new Point(columns * cell_size, rows * cell_size);
+        this.cells = [];
+        this.canvas = canvas;
+        this.canvas.attr({ width: this.columns * this.cell_size + 1, height: this.rows * this.cell_size + 1 });
+        this.context = canvas.get(0).getContext("2d");
+        for (var x = 0; x < rows; x++) {
+            var row = [];
+            for (var y = 0; y < columns; y++) {
+                row.push(new Cell(x, y, this));
+            }
+            this.cells.push(row);
+        }
+        var winner_x = getRandomInt(0, this.rows);
+        var winner_y = getRandomInt(0, this.columns);
+        this.winning_cell = this.cells[winner_x][winner_y];
+        this.winning_cell.state = CellState.Winner;
+        this.context.strokeStyle = "#000000";
+        this.context.font = (this.cell_size - 4).toString() + "px sans-serif";
+        this.context.textBaseline = "bottom";
+        this.context.textAlign = "center";
+        this.redraw();
+    }
+    Board.prototype.redraw = function () {
+        for (var _i = 0, _a = this.cells; _i < _a.length; _i++) {
+            var row = _a[_i];
+            for (var _b = 0, row_1 = row; _b < row_1.length; _b++) {
+                var cell = row_1[_b];
+                cell.draw();
+            }
+        }
+    };
+    Board.prototype.startTimer = function () {
+        this.timer = setInterval(gameTimer, 1000);
+    };
+    Board.prototype.stopTimer = function () {
+        clearInterval(this.timer);
+    };
+    Board.prototype.addMove = function () {
+        this.moves += 1;
+        $("#moves_value").text(this.moves);
+    };
+    return Board;
+}());
+var Cell = (function () {
+    function Cell(row, column, board) {
+        this.state = CellState.Unknown;
+        this.shown = false;
+        this.row = row;
+        this.column = column;
+        this.board = board;
+    }
+    Cell.initialize = function () {
+        this.drawData[CellState.Unknown] = new CellDrawData("#FFFFFF", "#000000", "?");
+        this.drawData[CellState.Up] = new CellDrawData("#00FB2A", "#000000", "🡩");
+        this.drawData[CellState.Down] = new CellDrawData("#FBEA00", "#000000", "🡫");
+        this.drawData[CellState.Left] = new CellDrawData("#00F3FB", "#000000", "🡨");
+        this.drawData[CellState.Right] = new CellDrawData("#FB0043", "#FFFFFF", "🡪");
+        this.drawData[CellState.Winner] = new CellDrawData("#000000", "#FFD700", "✖");
+    };
+    Cell.prototype.draw = function () {
+        var ctx = this.board.context;
+        var size = this.board.cell_size;
+        var x = this.column * size;
+        var y = this.row * size;
+        var data;
+        if (this.shown) {
+            data = Cell.drawData[this.state];
+        }
+        else {
+            data = Cell.drawData[CellState.Unknown];
+        }
+        ctx.strokeRect(x + 0.5, y + 0.5, size, size);
+        ctx.fillStyle = data.backgroundColor;
+        ctx.fillRect(x + 1, y + 1, size - 1, size - 1);
+        ctx.fillStyle = data.textColor;
+        ctx.fillText(data.text, x + (size / 2), y + size + 2);
+    };
+    Cell.prototype.click = function () {
+        if (this.board.state == GameState.Initial) {
+            this.board.startTimer();
+            this.board.state = GameState.Active;
+        }
+        if (!this.shown && this.board.state == GameState.Active) {
+            if (this.state == CellState.Unknown) {
+                var options = [];
+                if (this.row < this.board.winning_cell.row) {
+                    options.push(CellState.Down);
+                }
+                else if (this.row > this.board.winning_cell.row) {
+                    options.push(CellState.Up);
+                }
+                if (this.column < this.board.winning_cell.column) {
+                    options.push(CellState.Right);
+                }
+                else if (this.column > this.board.winning_cell.column) {
+                    options.push(CellState.Left);
+                }
+                if (options.length == 1) {
+                    this.state = options[0];
+                }
+                else {
+                    this.state = options[getRandomInt(0, options.length)];
+                }
+            }
+            this.board.addMove();
+            this.shown = true;
+            this.draw();
+            if (this.state == CellState.Winner) {
+                alert("Congratulations! Press 'Start' to play again.");
+                this.board.stopTimer();
+                this.board.state = GameState.Complete;
+            }
+        }
+    };
+    return Cell;
+}());
+Cell.drawData = [];
+var CellDrawData = (function () {
+    function CellDrawData(backgroundColor, textColor, text) {
+        this.backgroundColor = backgroundColor;
+        this.textColor = textColor;
+        this.text = text;
+    }
+    return CellDrawData;
+}());
+var Point = (function () {
+    function Point(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    return Point;
+}());
+var CellState;
+(function (CellState) {
+    CellState[CellState["Unknown"] = 0] = "Unknown";
+    CellState[CellState["Up"] = 1] = "Up";
+    CellState[CellState["Down"] = 2] = "Down";
+    CellState[CellState["Left"] = 3] = "Left";
+    CellState[CellState["Right"] = 4] = "Right";
+    CellState[CellState["Winner"] = 5] = "Winner";
+})(CellState || (CellState = {}));
+var GameState;
+(function (GameState) {
+    GameState[GameState["Initial"] = 0] = "Initial";
+    GameState[GameState["Active"] = 1] = "Active";
+    GameState[GameState["Complete"] = 2] = "Complete";
+})(GameState || (GameState = {}));
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+function gameTimer() {
+    board.duration += 1;
+    var duration = board.duration;
+    var minutes = Math.floor(duration / 60).toString();
+    if (minutes.length == 1) {
+        minutes = "0" + minutes;
+    }
+    var seconds = (duration % 60).toString();
+    if (seconds.length == 1) {
+        seconds = "0" + seconds;
+    }
+    $("#timer_value").text(minutes + ":" + seconds);
+}
+function CreateBoard() {
+    $("#timer_value").text("00:00");
+    $("#moves_value").text("0");
+    board = new Board(parseInt($("#board_rows").val()), parseInt($("#board_columns").val()), parseInt($("#cell_size").val()), $("#board"));
+}
+function CellClickHandler(e) {
+    var click = new Point(e.offsetX, e.offsetY);
+    var row = Math.floor(click.y / board.cell_size);
+    var column = Math.floor(click.x / board.cell_size);
+    var target_cell = board.cells[row][column];
+    target_cell.click();
+}
+var board;
+$(function () {
+    $("#game_controls").submit(function (e) {
+        e.preventDefault();
+        CreateBoard();
+    });
+    $("#board").click(CellClickHandler);
+    $("#game_controls").submit();
+});
---
+++ src/gridgame.ts
@@ -0,0 +1,226 @@
+class Board {
+    readonly rows: number;
+    readonly columns: number;
+    cell_size: number;
+    bounds: Point;
+    cells: Cell[][];
+    winning_cell: Cell;
+    canvas: JQuery;
+    context: CanvasRenderingContext2D;
+    duration = 0;
+    moves = 0;
+    timer: number;
+    state = GameState.Initial;
+
+    constructor(rows: number, columns: number, cell_size: number, canvas: JQuery) {
+        Cell.initialize();
+
+        this.rows = rows;
+        this.columns = columns;
+        this.cell_size = cell_size;
+        this.bounds = new Point(columns * cell_size, rows * cell_size);
+        this.cells = [];
+        this.canvas = canvas;
+        //Set board to correct size
+        this.canvas.attr({ width: this.columns * this.cell_size + 1, height: this.rows * this.cell_size + 1 });
+        this.context = (canvas.get(0) as HTMLCanvasElement).getContext("2d");
+
+        //Populate cells
+        for (let x = 0; x < rows; x++) {
+            let row: Cell[] = [];
+            for (let y = 0; y < columns; y++) {
+                row.push(new Cell(x, y, this));
+            }
+            this.cells.push(row);
+        }
+
+        //Pick winning cell
+        let winner_x = getRandomInt(0, this.rows);
+        let winner_y = getRandomInt(0, this.columns);
+        this.winning_cell = this.cells[winner_x][winner_y];
+        this.winning_cell.state = CellState.Winner;
+
+
+        //Set default styles for canvas
+        this.context.strokeStyle = "#000000";
+        //this.context.font = "16px sans-serif";
+        this.context.font = (this.cell_size - 4).toString() + "px sans-serif";
+        this.context.textBaseline = "bottom";
+        this.context.textAlign = "center";
+        this.redraw();
+    }
+    redraw() {
+        for (let row of this.cells) {
+            for (let cell of row) {
+                cell.draw();
+            }
+        }
+    }
+    startTimer() {
+        this.timer = setInterval(gameTimer, 1000);
+    }
+    stopTimer() {
+        clearInterval(this.timer);
+    }
+    addMove() {
+        this.moves += 1;
+        $("#moves_value").text(this.moves);
+    }
+}
+class Cell {
+    readonly row: number;
+    readonly column: number;
+    board: Board;
+    state: CellState = CellState.Unknown;
+    static drawData: CellDrawData[] = [];
+    shown: boolean = false;
+
+    static initialize() {
+        this.drawData[CellState.Unknown] = new CellDrawData("#FFFFFF", "#000000", "?");
+        this.drawData[CellState.Up] = new CellDrawData("#00FB2A", "#000000", "🡩");
+        this.drawData[CellState.Down] = new CellDrawData("#FBEA00", "#000000", "🡫");
+        this.drawData[CellState.Left] = new CellDrawData("#00F3FB", "#000000", "🡨");
+        this.drawData[CellState.Right] = new CellDrawData("#FB0043", "#FFFFFF", "🡪");
+        this.drawData[CellState.Winner] = new CellDrawData("#000000", "#FFD700", "✖");
+    }
+
+    constructor(row: number, column: number, board: Board) {
+        this.row = row;
+        this.column = column;
+        this.board = board;
+    }
+    draw() {
+        let ctx = this.board.context;
+        let size = this.board.cell_size;
+
+        let x = this.column * size;
+        let y = this.row * size;
+
+        //Get draw data
+        let data: CellDrawData;
+        if (this.shown) {
+            data = Cell.drawData[this.state];
+        }
+        else {
+            data = Cell.drawData[CellState.Unknown];
+        }
+        //Draw bounding box
+        //+0.5 is required to make browser correctly draw lines, see http://diveintohtml5.info/canvas.html#paths for more info
+        ctx.strokeRect(x + 0.5, y + 0.5, size, size);
+        //Fill box
+        ctx.fillStyle = data.backgroundColor;
+        ctx.fillRect(x + 1, y + 1, size - 1, size - 1);
+        //Draw text inside
+        ctx.fillStyle = data.textColor;
+        ctx.fillText(data.text, x + (size / 2), y + size + 2);
+
+    }
+    click() {
+        if (this.board.state == GameState.Initial) {
+            this.board.startTimer();
+            this.board.state = GameState.Active;
+        }
+        if (!this.shown && this.board.state == GameState.Active) {
+            if (this.state == CellState.Unknown) {
+                let options: CellState[] = []
+
+                if (this.row < this.board.winning_cell.row) {
+                    options.push(CellState.Down);
+                }
+                else if (this.row > this.board.winning_cell.row) {
+                    options.push(CellState.Up);
+                }
+                if (this.column < this.board.winning_cell.column) {
+                    options.push(CellState.Right);
+                }
+                else if (this.column > this.board.winning_cell.column) {
+                    options.push(CellState.Left);
+                }
+
+                if (options.length == 1) {
+                    this.state = options[0];
+                }
+                else {
+                    this.state = options[getRandomInt(0, options.length)];
+                }
+            }
+            this.board.addMove();
+            this.shown = true;
+            this.draw();
+
+            if (this.state == CellState.Winner) {
+                alert("Congratulations! Press 'Start' to play again.");
+                this.board.stopTimer();
+                this.board.state = GameState.Complete;
+            }
+        }
+    }
+}
+class CellDrawData {
+    backgroundColor: string;
+    textColor: string;
+    text: string;
+    constructor(backgroundColor: string, textColor: string, text: string) {
+        this.backgroundColor = backgroundColor;
+        this.textColor = textColor;
+        this.text = text;
+    }
+}
+class Point {
+    x: number;
+    y: number;
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+}
+enum CellState {
+    Unknown, Up, Down, Left, Right, Winner
+}
+enum GameState {
+    Initial, Active, Complete
+}
+function getRandomInt(min: number, max: number): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+function gameTimer() {
+    board.duration += 1;
+    let duration = board.duration;
+    let minutes = Math.floor(duration / 60).toString();
+    if (minutes.length == 1) {
+        minutes = "0" + minutes;
+    }
+    let seconds = (duration % 60).toString();
+    if (seconds.length == 1) {
+        seconds = "0" + seconds;
+    }
+    $("#timer_value").text(minutes + ":" + seconds);
+}
+function CreateBoard() {
+    $("#timer_value").text("00:00");
+    $("#moves_value").text("0");
+    board = new Board(parseInt($("#board_rows").val()), parseInt($("#board_columns").val()), parseInt($("#cell_size").val()), $("#board"));
+}
+
+function CellClickHandler(e) {
+    let click = new Point(e.offsetX, e.offsetY);
+    let row = Math.floor(click.y / board.cell_size);
+    let column = Math.floor(click.x / board.cell_size);
+    let target_cell = board.cells[row][column];
+    target_cell.click();
+}
+
+var board: Board;
+
+$(function() {
+    $("#game_controls").submit(function(e) {
+        e.preventDefault();
+        CreateBoard();
+    });
+
+    $("#board").click(CellClickHandler);
+
+    $("#game_controls").submit();
+});
